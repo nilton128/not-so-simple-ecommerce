a47d659f80d28445f7e64ba694b26a7c0875b98e
Nilton Rodrigues <nilton128@gmail.com>
2024-12-02 14:22:46 -0300

    productStockQueue

--- iac/main.tf
+++ iac/main.tf
@@ -22,3 +22,14 @@ resource "aws_sqs_queue" "emailNotificationQueue" {
     Environment = "Local"
   }
 }
+
+resource "aws_sqs_queue" "productStockQueue" {
+  name                      = "ProductStockQueue"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+  tags = {
+    Environment = "Local"
+  }
+}
