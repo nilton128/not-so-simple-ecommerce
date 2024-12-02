f67a98c76de67313ba897cef69d06b0395851f94
Nilton Rodrigues <nilton128@gmail.com>
2024-12-02 14:21:56 -0300

    aws_sqs_queue

--- iac/main.tf
+++ iac/main.tf
@@ -11,3 +11,14 @@ provider "aws" {
     sqs             = "http://nsse.localstack.internal:4566"
   }
 }
+
+resource "aws_sqs_queue" "emailNotificationQueue" {
+  name                      = "EmailNotificationQueue"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+  tags = {
+    Environment = "Local"
+  }
+}
