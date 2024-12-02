376d26df632ea7232ca11a1fece330ca439c245b
Nilton Rodrigues <nilton128@gmail.com>
2024-12-02 14:23:27 -0300

    invoiceQueue

--- iac/main.tf
+++ iac/main.tf
@@ -33,3 +33,14 @@ resource "aws_sqs_queue" "productStockQueue" {
     Environment = "Local"
   }
 }
+
+resource "aws_sqs_queue" "invoiceQueue" {
+  name                      = "InvoiceQueue"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+  tags = {
+    Environment = "Local"
+  }
+}
